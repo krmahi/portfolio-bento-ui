571985a40b6c4d3609caba21efb98364263d2b9a
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-02-12 19:29:26 +0530

    add: endnote showing {Figma} design file and status

---
+++ src/components/Endnote.tsx
@@ -0,0 +1,25 @@
+import "../styles/Endnote.css";
+
+function Endnote() {
+  return (
+    <div className="note-div">
+      <div className="note-text">
+        under development | Design file:{" "}
+        <a
+          href="https://www.figma.com/file/f6NJxfrNd0pAEdc4Xeysg2/Bento-ui-%7C-portfolio?type=design&node-id=0%3A1&mode=design&t=fUEftutfbbuN1tgT-1 "
+          target="_blank"
+          rel="noopener"
+        >
+          Figma
+        </a>{" "}
+        Profile:{" "}
+        <a href="https://www.figma.com/@mahi3 " target="_blank" rel="noopener">
+          Figma
+        </a>{" "}
+        (More work)
+      </div>
+    </div>
+  );
+}
+
+export default Endnote;
