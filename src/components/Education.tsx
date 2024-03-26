0bc3a8ac1c7caa64a6fcfe343a12e1bebcdb3f7e
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-03-26 18:04:35 +0530

    add: Education

---
+++ src/components/Education.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../styles/Education.css";
+
+function Education() {
+  return (
+    <div className="experience-hero">
+      <div className="title">Education</div>
+      <div className="work-blocks">
+        <div className="block-1 block-color">
+          <div className="block-title edu-span">
+            GLBITM <span>07/2024</span>
+          </div>
+          <div className="block-sub-title">Computer Science</div>
+          <div className="block-sub-title edu-sub-title">
+            Delhi NCR, INDIA | 8.26 cgpa
+          </div>
+        </div>
+        <div className="block-1 block-color">
+          <div className="block-title edu-span">
+            Army public school <span>03/2020</span>
+          </div>
+          <div className="block-sub-title">Science, INTERMEDIATE</div>
+        </div>
+        <div className="block-1 block-color">
+          <div className="block-title edu-span">
+            Army public school <span>03/2018</span>
+          </div>
+          <div className="block-sub-title">Hign School</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Education;
