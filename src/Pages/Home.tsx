72836c52ffadeac6d5c2dd2dcf0a5b9ef260b404
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-06-01 20:29:40 +0530

    fix: production-ready fixes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Header from "../components/Header";
