1d4c44b6ecded3f23b3c3cbee5ea319d8a7f95f9
krmahi <mahesh.kr.2277@gmail.com>
2025-04-16 21:03:15 +0530

    add: latest experience and projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,5 @@
 // import React from "react";
 import "../styles/Projects.css";
-import voicegenix from "../assets/voicegenix.svg";
 
 function Projects() {
   const handleCodeP1 = () => {
@@ -12,11 +11,13 @@ function Projects() {
   const handleCodeP2 = () => {
     window.open("https://github.com/krmahi/portfolio-bento-ui");
   };
-  const handleViewP3 = () => {
-    window.open("https://krmahi.github.io/2d_ray_casting");
-  };
+  // const handleViewP3 = () => {
+  //   window.open("");
+  // };
   const handleCodeP3 = () => {
-    window.open("https://github.com/krmahi/2d_ray_casting");
+    window.open(
+      "https://github.com/krmahi/AI-powered-practical-situation-solver"
+    );
   };
   const handleViewP4 = () => {
     window.open(
@@ -27,13 +28,22 @@ function Projects() {
     window.open("https://github.com/krmahi/Gender-recognition-using-voice-GUI");
   };
   const handleViewP5 = () => {
-    window.open("https://krmahi.github.io/INSTAGRAM_FRONTEND_CLONE");
+    window.open("https://krmahi.github.io/2d_ray_casting/");
   };
   const handleCodeP5 = () => {
-    window.open("https://github.com/krmahi/INSTAGRAM_FRONTEND_CLONE");
+    window.open("https://github.com/krmahi/2d_ray_casting");
   };
   const handleCodeP6 = () => {
+    window.open("https://github.com/krmahi/SecondLife");
+  };
+  const handleCodeP7 = () => {
     window.open("https://github.com/krmahi/Volume-control-using-hand-gestures");
+  };
+  const handleViewP8 = () => {
+    window.open("https://31currencies-converter.vercel.app/");
+  };
+  const handleCodeP8 = () => {
+    window.open("https://github.com/krmahi/currency-converter");
   };
 
   return (
@@ -49,10 +59,11 @@ function Projects() {
           </div>
           <div className="size-15px bold-content ">
             <span>
-              Led dynamic simulation scenario creation with Lidar, INS, and
-              camera sensors in Unreal Engine, achieving Level 5 autonomy, and
-              developed real-time algorithms for traffic light recognition and
-              decision-making planners
+              Led the creation of dynamic simulation scenarios using Lidar, INS,
+              and camera sensors in Unreal Engine, achieving Level 5 autonomy.
+              Developed real-time algorithms for traffic light recognition and
+              decision-making planners, enabling the vehicle to navigate complex
+              environments with full autonomy
             </span>
             <div className="project-buttons">
               <div
@@ -81,9 +92,11 @@ function Projects() {
           </div>
           <div className="size-15px bold-content">
             <span>
-              Designed and deployed a dynamic React portfolio with a Bento-style
-              UI, showcasing skills, projects, and achievements through a clean
-              and organized layout for a modern and scalable web presence
+              Designed and deployed a dynamic React portfolio with a sleek
+              Bento-style UI, effectively showcasing skills, projects, and
+              achievements in a clean, organized layout. The responsive design
+              ensures a modern and scalable web presence, offering an engaging
+              user experience across devices
             </span>
             <div className="project-buttons">
               <div className="p1-button bold-content" onClick={handleViewP2}>
@@ -108,22 +121,24 @@ function Projects() {
         </div>
         <div className="block-1 project-block-color-3 c">
           <div className="block-title bold block-title-proj">
-            2D Ray tracing
+            AI Problem solver
           </div>
           <div className="block-sub-title bold-subheading size-15px block-sub-title-proj">
-            ray casting
+            AI - Drawing Calculator
           </div>
           <div className="size-15px bold-content">
             <span>
-              Implemented ray casting for realistic light simulation, utilizing
-              vector math to calculate ray-object intersections, determine
-              reflected ray directions, and calculate shadows by tracing rays
-              from intersections towards light sources for occlusion checks
+              An interactive calculator app designed to solve complex math and
+              real-world situation-based problems using natural language input.
+              Users can write descriptive queries or draw problem scenarios
+              directly within the app. It intelligently interprets inputs and
+              delivers accurate, step-by-step solutions in real time the app
+              offers an intuitive and seamless problem-solving experience
             </span>
             <div className="project-buttons ">
               <div
                 className="p1-button bold-content p3-button-color"
-                onClick={handleViewP3}
+                // onClick={handleViewP3}
               >
                 <a className="p3-a">View Site</a>
               </div>
@@ -136,12 +151,15 @@ function Projects() {
             </div>
           </div>
           <div className="categories normal-text">
-            <span>Vectors</span>
-            <span>Js</span>
-            <span>Html</span>
-            <span>Css</span>
+            <span>NLP</span>
+            <span>AI/ML</span>
+            <span>Real-Time Data Processing</span>
+            <span>css</span>
+            <span>UI/UX Design</span>
+            <span>Api</span>
           </div>
         </div>
+
         <div className="block-1 project-block-color-4 d">
           <div className="block-title bold block-title-proj">
             Gender recognition using Voice
@@ -152,10 +170,12 @@ function Projects() {
 
           <div className="size-15px bold-content ">
             <span>
-              Researched and implemented feature extraction techniques for
-              gender classification from voice data, designed an intuitive GUI
-              for VoiceGenix, and established a dynamic real-time data
-              collection strategy for continuous system adaptation
+              Researched and implemented advanced feature extraction techniques
+              for gender classification from voice data. Designed an intuitive
+              GUI for VoiceGenix, enhancing user interaction and system
+              accessibility. Established a dynamic, real-time data collection
+              strategy, enabling continuous adaptation and improvement of the
+              system’s performance
             </span>
 
             <div className="project-buttons">
@@ -171,7 +191,6 @@ function Projects() {
               >
                 <a>Source Code</a>
               </div>
-              <img src={voicegenix} alt="voicegenix_logo" />
             </div>
           </div>
           <div className="categories">
@@ -183,50 +202,165 @@ function Projects() {
             <span>Tkinter</span>
           </div>
         </div>
+
         <div className="block-1 project-block-color-5 e">
           <div className="block-title bold block-title-proj">
-            Instagram Front-end Clone
+            2D Ray tracing
           </div>
-          <div className="last-project">
-            <div className="block-sub-title bold-subheading size-15px last-2-subheading block-sub-title-proj">
-              html | css | figma
-            </div>
-            <div className="size-15px">
-              <div className="project-buttons margin-no-bottom">
-                <div
-                  className="p1-button bold-content p5-button-color"
-                  onClick={handleViewP5}
-                >
-                  <a>View Site</a>
-                </div>
-                <div
-                  className="p1-button bold-content p5-button-color"
-                  onClick={handleCodeP5}
-                >
-                  <a>Source Code</a>
-                </div>
+          <div className="block-sub-title bold-subheading size-15px block-sub-title-proj">
+            2d ray casting
+          </div>
+          <div className="size-15px bold-content ">
+            <span>
+              Implemented a 2D ray casting system to simulate realistic lighting
+              by calculating ray-object intersections using vector mathematics.
+              Determined reflection paths and handled shadow rendering by
+              tracing rays from hit points to light sources with occlusion
+              detection. Achieved visually accurate light behavior for dynamic
+              scene interactions
+            </span>
+
+            <div className="project-buttons">
+              <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleViewP5}
+              >
+                <a>View Site</a>
+              </div>
+              <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleCodeP5}
+              >
+                <a>Source Code</a>
               </div>
             </div>
+          </div>
+          <div className="categories">
+            <span>Vector</span>
+            <span>3D Graphics Theory (2D simulation)</span>
+            <span>js</span>
+            <span>Lighting and Shadow Simulation</span>
           </div>
         </div>
-        <div className="block-1 project-block-color-5 f">
-          <div className="block-title bold block-title-proj ">
-            System volume control using hand Gestures
+
+        <div className="block-1 project-block-color-6 f">
+          <div className="block-title bold block-title-proj">
+            Second Life App
           </div>
-          <div className="last-project">
-            <div className="block-sub-title bold-subheading size-15px last-2-subheading block-sub-title-proj">
-              computer vision | python | gesture recognition
-            </div>
-            <div className="size-15px">
-              <div className="project-buttons margin-no-bottom">
-                <div
-                  className="p1-button bold-content p5-button-color"
-                  onClick={handleCodeP6}
-                >
-                  <a>Source Code</a>
-                </div>
+          <div className="block-sub-title bold-subheading size-15px block-sub-title-proj">
+            Buy and Sell items
+          </div>
+          <div className="size-15px bold-content ">
+            <span>
+              Second Life App is a cross-platform marketplace built with React
+              Native for buying and selling pre-owned items. It features an
+              intuitive UI for easy item listings, browsing, and secure
+              transactions. The app promotes sustainable reuse by connecting
+              users to give items a second life. It also includes chat
+              functionality and secure payment integration for seamless
+              transactions
+            </span>
+            <div className="project-buttons">
+              {/* <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleViewP4}
+              >
+                <a>Releases</a>
+              </div> */}
+              <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleCodeP6}
+              >
+                <a>Source Code</a>
               </div>
             </div>
+          </div>
+          <div className="categories">
+            <span>React Native</span>
+            <span>JavaScript</span>
+            <span>Pyton</span>
+            <span>OAuth</span>
+            <span>real-time chat</span>
+            <span>UI/UX Design (Figma, Adobe XD)</span>
+          </div>
+        </div>
+
+        <div className="block-1 project-block-color-7 g">
+          <div className="block-title bold block-title-proj">
+            System volume control using hand Gestures
+          </div>
+          <div className="block-sub-title bold-subheading size-15px block-sub-title-proj">
+            volume control
+          </div>
+          <div className="size-15px bold-content ">
+            <span>
+              This system uses OpenCV for real-time hand gesture recognition to
+              control the computer's volume. By tracking hand movements via
+              webcam, users can adjust the volume up or down intuitively,
+              without needing to physically interact with the device.
+            </span>
+
+            <div className="project-buttons">
+              {/* <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleViewP4}
+              >
+                <a>Releases</a>
+              </div> */}
+              <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleCodeP7}
+              >
+                <a>Source Code</a>
+              </div>
+            </div>
+          </div>
+          <div className="categories">
+            <span>OpenCV</span>
+            <span>Gesture Recognition</span>
+            <span>Pyton</span>
+            <span>Research & Development</span>
+            <span>Real-Time Processing</span>
+          </div>
+        </div>
+
+        <div className="block-1 project-block-color-8 h">
+          <div className="block-title bold block-title-proj">
+            Currency converter
+          </div>
+          <div className="block-sub-title bold-subheading size-15px block-sub-title-proj">
+            30+ Currencies
+          </div>
+          <div className="size-15px bold-content ">
+            <span>
+              A ReactJS app supporting 30+ currencies with real-time exchange
+              rates. Users can save favorite currencies and preferences using
+              local storage. Offers seamless currency conversion with a clean,
+              intuitive interface
+            </span>
+
+            <div className="project-buttons">
+              <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleViewP8}
+              >
+                <a>View Site</a>
+              </div>
+              <div
+                className="p1-button bold-content p4-button-color"
+                onClick={handleCodeP8}
+              >
+                <a>Source Code</a>
+              </div>
+            </div>
+          </div>
+          <div className="categories">
+            <span>ReactJS</span>
+            <span>TypeScript</span>
+            <span>Local Storage</span>
+            <span>Currency Formatting</span>
+            <span>State Management (Context API)</span>
+            <span>UI/UX Design</span>
           </div>
         </div>
       </div>
